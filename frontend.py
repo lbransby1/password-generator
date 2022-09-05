09f5ad8d4b83f220670833ab210fed0ef998e41d
lbransby1 <luke.bransby15@gmail.com>
2022-09-05 14:50:17 +0100

    try

--- frontend.py
+++ frontend.py
@@ -1,41 +1,71 @@
 import tkinter as tk
+from password import * 
 
 def destroy():
   for widget in root.winfo_children():
     widget.destroy()
 
+
+
+# home 
 def home(root):
   destroy()
   page = tk.Frame(root)
   page.grid()
-  tk.Label(page, text = 'This is the home page of the program').grid(row = 0, columnspan=2)
-  tk.Button(page, text = 'Enter new details', command = lambda:new(root)).grid(row = 1)
-  tk.Button(page, text = 'View previous passwords', command = lambda:view(root)).grid(row = 1, column=1)
 
+  tk.Label(page, text = 'This is the home page of the program').grid(row = 0, columnspan=2)
+  
+  tk.Button(page, text = 'Enter new details', command = lambda:new(root)).grid(row = 2)
+  
+  tk.Button(page, text = 'View previous passwords', command = lambda:view(root)).grid(row = 2, column=1)
+
+# end of home 
+
+
+#new
 def new(root):
   destroy()
   page = tk.Frame(root)
   page.grid()
-  tk.Label(page, text = 'This is where the new details will be added').grid(row = 0)
-  tk.Button(page, text = 'Back to home', command = lambda:home(root)).grid(row = 1)
+  
+  tk.Label(page, text = 'This is where the new details will be added').grid(row = 0, columnspan=3)
+  
+  website = tk.StringVar()
+  tk.Label(page, text = 'Website').grid(column=0, row=1)
+  tk.Entry(page, textvariable=website).grid(column=1, row=1)
 
+  username = tk.StringVar()
+  tk.Label(page, text = 'Username').grid(column=0, row=2)
+  tk.Entry(page, textvariable=username).grid(column=1, row=2)
+
+  password = tk.StringVar()
+  tk.Label(page, text = 'Password').grid(column=0, row=3)
+  tk.Entry(page, textvariable=password, show='#').grid(column=1, row=3)
+  tk.Button(page, text='Generate password', command=lambda:passwordPage()).grid(column=2, row=3)
+
+
+  tk.Button(page, text = 'Back to home', command = lambda:home(root)).grid(row = 100)
+
+#end of new
+
+#view
 def view(root):
   destroy()
   page = tk.Frame(root)
   page.grid()
+  
   tk.Label(page, text = 'This is the page for viewing details').grid(row = 0)
+  
   tk.Button(page, text = 'Back', command = lambda:home(root)).grid(row = 1)
+#end of view
 
-def changepage(page):
-  global root
-  for widget in root.winfo_children():
-    widget.destroy()
-  if page == new:
-    new(root)
-  elif page == home:
-    home(root)
-    
+
+
+
+
+
 
 root = tk.Tk()
+root.title('Password Manager')
 home(root)
 root.mainloop()
---
+++ password.py
@@ -0,0 +1,72 @@
+import tkinter as tk
+import random
+
+def baseLetters():
+  letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+  pw = ""
+  for i in range(0, 8):
+    newLetter = letters[random.randrange(0, len(letters))]
+    pw = str(pw + newLetter)
+    i = i+1 
+  return pw
+  
+def addNumbers():
+  newPassword = ''
+  for i in range(3):
+    newPassword += str(random.randrange(0,9))
+    i = i+1
+  return(newPassword)
+
+def capPassword(pw):
+  return(pw.capitalize())
+
+def specialChar():
+  letters = ['!','£','$','.',',','@',':',';','/','?','#']
+  pw = str(letters[random.randint(0, len(letters)-1)])
+  return pw
+
+
+def passwordPage():
+
+  def generatePassword():
+    password = baseLetters()
+    if numBool.get() == 'True':
+      password += addNumbers()
+    if capBool.get() =='True':
+      password = capPassword(password)
+    if charBool.get() == 'True':
+      password += specialChar()
+    print(password)
+  
+  def printIfClicked():
+    if numBool:
+      print(numBool)
+    
+
+  pw=tk.Tk()
+  pw.title('Password generator and validator')
+
+
+
+  pw.resizable(False, False)
+  pw.title('Password Generator and Validator')
+  pw.columnconfigure(0)
+  pw.rowconfigure(0)
+
+
+  numBool= tk.StringVar()
+  tk.Checkbutton(pw, text="Add numbers", onvalue="True", offvalue="False", variable= numBool).grid(column=0, row = 0)
+
+  capBool= tk.StringVar()
+  tk.Checkbutton(pw, text="Capitalize", onvalue="True", offvalue="False", variable= capBool).grid(column=1, row = 0)
+
+  charBool= tk.StringVar()
+  tk.Checkbutton(pw, text="special character", onvalue="True", offvalue="False", variable= charBool, command=printIfClicked).grid(column=2, row = 0)
+
+  tk.Button(pw, text="Generate Password", command=lambda:generatePassword()).grid(column=0, row=1)
+
+
+  pw.mainloop()
+
+
+
